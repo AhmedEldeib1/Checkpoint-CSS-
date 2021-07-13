cd89f4ae1a2a8f15ac435de960b68852e49bf3e4
AhmedEldeib1 <ahmed0eldeib@gmail.com>
2021-07-13 20:23:28 +0200

    First commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
 
+// let myJSON='{"name": "Ahmed", "age":20,"City":"Zefta"}';
+// let myJS=JSON.parse(myJSON);
 
+// console.log(myJS);
+// console.log("my name:"+myJS.name);
+// console.log("my age:"+myJS.age);
+// console.log("my city:"+myJS.City);
+
+// fetch("../data-type.json")
+//     .then (function(project){
+//         return project.json();
+//     })
+//     .then(function(mydata){
+//         console.log(mydata);
+//     })
+
+
+
+// fetch("../data-type.json")
+//     .then(resp=>resp.json())
+//     .then(data=>console.log("My name: "+data.name));
 
